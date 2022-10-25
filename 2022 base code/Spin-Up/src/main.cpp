a46ba57477c14adb0150cc0832a816776de7574f
Kelvin <kelvinvillago@gmail.com>
2022-10-25 10:21:47 -0700

    Update main.cpp
    Updated shooter code for testing during the week. Added comments for functionality of each part.

--- 2022 base code/Spin-Up/src/main.cpp
+++ 2022 base code/Spin-Up/src/main.cpp
@@ -19,24 +19,20 @@ const int8_t SHOOTER_PORT2 = 15;
 pros::Imu gyro(GYRO_PORT);
 double gyro_offset = 0;
 
-double getRawRotation() {
+//helper functions to work with field-centric x-drive
+double getRawRotation() { //get data from the Vex IMU
 	double heading = gyro.get_heading();
 	return (heading == PROS_ERR_F ? 0 : heading);
 }
 
-double getRotation() {
+double getRotation() { //subtracts an offset from the gyro value to stabilize
 	return getRawRotation() - gyro_offset;
 }
 
 void resetRotation() {
 	gyro_offset = getRawRotation();
 }
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -47,64 +43,18 @@ void on_center_button() {
 	}
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
 	resetRotation();
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void autonomous() {}
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
@@ -117,9 +67,6 @@ void opcontrol() {
 	pros::Motor Shooter2(SHOOTER_PORT2);
 	int motorOn = 0;
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
 
 		//field centric x-drive
 		int x = master.get_analog(ANALOG_LEFT_X);
@@ -138,32 +85,35 @@ void opcontrol() {
 		std::string print_angle = std::to_string(angle * 180 / M_PI);
 		pros::lcd::set_text(2, "Angle: " + print_angle);
 
-		pros::lcd::set_text(3, "RPM: " + std::to_string(18*Shooter2.get_actual_velocity()));
+		pros::lcd::set_text(3, "RPM1: " + std::to_string(18*Shooter2.get_actual_velocity())); //print rpm of shooter motors for testing
+		pros::lcd::set_text(4, "RPM2: " + std::to_string(18*Shooter1.get_actual_velocity()));
 
-		if(master.get_digital(DIGITAL_R1)) {
+		if(master.get_digital(DIGITAL_R1)) { //turn on shooter in one direction
 			motorOn = 1;
 		}
-		else if(master.get_digital(DIGITAL_R2)) {
+		else if(master.get_digital(DIGITAL_R2)) { //turn off shooter
 			motorOn = 0;
 		}
-		else if(master.get_digital(DIGITAL_L1)) {
+		else if(master.get_digital(DIGITAL_L1)) { //turn on shooter in other direction for testing
 			motorOn = 2;
 		}
 
-		if (motorOn == 1) {
+		//set motor voltages to values to either max or zero voltage
+		if (motorOn == 0) {
+			Shooter1.move_voltage(0);
+			Shooter2.move_voltage(0);
+		}
+		else if (motorOn == 1) {
  			Shooter1.move_voltage(12000);
  			Shooter2.move_voltage(-12000);
-		} else if (motorOn == 0) {
- 			Shooter1.move_voltage(0);
- 			Shooter2.move_voltage(0);
-		} else if(motorOn == 2) {
+		}
+		else if(motorOn == 2) {
 	 		Shooter1.move_voltage(-12000);
 	 		Shooter2.move_voltage(12000);
 		}
 
-		if(master.get_digital_new_press(DIGITAL_A))
-		{
-			gyro.reset();
+		if(master.get_digital_new_press(DIGITAL_A)) {
+			gyro.reset(); //manual reset of gyro for testing
 		}
 
 		//X-drive
